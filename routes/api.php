977f8e631384f027a99cf76af307b35c4cb05839
Yesaya R. Athuman <yesayaathuman@gmail.com>
2019-03-14 17:37:25 +0300

    Remove auth protection.

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware("auth:api")->get("/posts", "Api\PostController@index");
+Route::get("/posts", "Api\PostController@index");
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
