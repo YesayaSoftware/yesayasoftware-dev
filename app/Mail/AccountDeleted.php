2d5d0fe60801cf4cb5ce3ae498c274f0fd923a47
Yesaya R. Athuman <yesayaathuman@gmail.com>
2019-07-15 07:01:12 +0300

    Update yarn

--- app/Mail/AccountDeleted.php
+++ app/Mail/AccountDeleted.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class AccountDeleted extends Mailable
+class AccountDeleted extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
